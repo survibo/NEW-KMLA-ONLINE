8fcc572274e616023dcc52744959a9b301189d4a
Jeonguk Choi <dev@cjeong.uk>
2026-05-24 22:20:04 +0900

    Fix overscroll problem

--- app/components/layout/app-shell.tsx
+++ app/components/layout/app-shell.tsx
@@ -12,12 +12,12 @@ type AppShellProps = {
 export function AppShell({ email }: AppShellProps) {
   return (
     <SidebarProvider defaultOpen={false}>
-      <div className="flex h-svh w-full flex-1 flex-col overflow-hidden">
+      <div className="flex h-svh w-full flex-1 flex-col overflow-hidden overscroll-none">
         <AppHeader email={email} />
         <div className="flex min-h-0 flex-1">
           <AppSidebar />
           <SidebarInset className="min-h-0">
-            <div className="bg-muted/25 flex flex-1 flex-col overflow-y-auto p-4 pb-24 sm:p-6 md:pb-6">
+            <div className="flex flex-1 flex-col overflow-y-auto overscroll-contain p-4 pb-24 sm:p-6 md:pb-6">
               <Outlet />
             </div>
           </SidebarInset>
